014dea150a073e9e630d8be22ccc1fdbc3d888fb
Dylan Carter <167344346+dylcarinc@users.noreply.github.com>
2026-03-06 15:32:10 -0600

    Added validation to fields

--- RazorPagesLab/Models/City.cs
+++ RazorPagesLab/Models/City.cs
@@ -6,14 +6,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 public class City
 {
     public int Id { get; set; }
+    [Required]
     public string? Name { get; set; }
     [DataType(DataType.Date)]
     [Display(Name = "Date Visited")]
+    [Required]
     public DateTime DateVisited { get; set; }
     [Display(Name = "State or Region")]
+    [Required]
      public string? StateRegion  { get; set; }
+    [Required] 
     public string? Country { get; set; }
+    [Required]
+    [DisplayFormat(DataFormatString = "{0:N0}")]
     public int Population { get; set; }
+    [Required]
     [Display(Name = "Days Spent")]
     public int DaysSpent { get; set; }
 
