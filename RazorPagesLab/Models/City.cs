2e923cb499a43d926eb63dc872f2c0871bd76b4f
Dylan Carter <167344346+dylcarinc@users.noreply.github.com>
2026-03-06 14:14:29 -0600

    part 5, changed column titles

--- RazorPagesLab/Models/City.cs
+++ RazorPagesLab/Models/City.cs
@@ -1,13 +1,16 @@
 using System.ComponentModel.DataAnnotations;
 
 namespace RazorPagesLab.Models;
+using System.ComponentModel.DataAnnotations.Schema;
 
 public class City
 {
     public int Id { get; set; }
     public string? Name { get; set; }
     [DataType(DataType.Date)]
+    [Display(Name = "Date Visited")]
     public DateTime DateVisited { get; set; }
+    [Display(Name = "State or Region")]
      public string? StateRegion  { get; set; }
     public string? Country { get; set; }
     public int Population { get; set; }
